8f5c7700dc2cb45eb0da05a29f1b3d8457ad7cc3
Yevhenii Herasymenko <zhenya36546@gmail.com>
2025-12-29 15:43:06 +0200

    added new styles

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,31 @@
 import '../scss/style.scss'
 switchThemeColor()
+
+(function () {
+  const isOpen = document.querySelector(".menu__burger");
+  const isClose = document.querySelector(".menu__cross");
+  const navigation = document.querySelector(".menu");
+
+  if (isOpen && navigation) {
+    isOpen.addEventListener("click", () => {
+      navigation.classList.toggle("active");
+    });
+  }
+
+  if (isClose && navigation) {
+    isClose.addEventListener("click", () => {
+      navigation.classList.remove("active");
+    });
+  }
+
+  document.addEventListener("click", (event) => {
+    if (
+      navigation &&
+      !navigation.contains(event.target) &&
+      isOpen &&
+      !isOpen.contains(event.target)
+    ) {
+      navigation.classList.remove("active");
+    }
+  });
+})();
