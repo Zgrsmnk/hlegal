8b6627debcbb92890ab1e1a7285d999702ac6577
Yevhenii Herasymenko <zhenya36546@gmail.com>
2026-01-07 18:59:36 +0200

    searching for bugs

--- src/js/main.js
+++ src/js/main.js
@@ -28,3 +28,27 @@ import '../scss/style.scss'
     }
   });
 })();
+
+(function () {
+  // Отримуємо всі елементи вкладок
+  const tabs = document.querySelectorAll(".tabs__link");
+
+  // Отримуємо всі елементи контенту, які відповідають вкладкам
+  const contents = document.querySelectorAll(".tabs__content-text");
+
+  // Для кожної вкладки в масиві "tabs" створюємо обробник події
+  tabs.forEach((tab, index) => {
+    // Додаємо подію на клік по вкладці
+    tab.addEventListener("click", () => {
+      // Видаляємо клас "active" у всіх вкладок та всіх елементів контенту
+      tabs.forEach((n) => n.classList.remove("active"));
+      contents.forEach((m) => m.classList.remove("active"));
+
+      // Додаємо клас "active" до вибраної вкладки (та яка була натиснута)
+      tab.classList.add("active");
+
+      // Додаємо клас "active" до відповідного контенту, що відповідає вкладці
+      contents[index].classList.add("active");
+    });
+  });
+})();
